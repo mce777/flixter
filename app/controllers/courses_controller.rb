b83aa8805da83395abef1d575c8cef68fdca98a6
mce777 <mce777@gmail.com>
2015-10-28 20:33:29 +0000

    course show page

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,9 +1,13 @@
 class CoursesController < ApplicationController
   
-  # cources controller FOR STUDENTS !!!
+  # courses controller !!! FOR STUDENTS !!!
   
   def index
     @courses = Course.all
+  end
+
+  def show
+  	@course = Course.find(params[:id])
   end
 
 end
